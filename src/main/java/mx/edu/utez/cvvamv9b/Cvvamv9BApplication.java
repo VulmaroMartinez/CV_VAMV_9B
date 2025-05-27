38d065fc1010cca7205b07252ab5887c62c6b7bd
VulmaroMartinez <20223tn069@utez.edu.mx>
2025-05-27 15:52:29 -0600

    [FIRST COMMIT] Subida inicial del proyecto

---
+++ src/main/java/mx/edu/utez/cvvamv9b/Cvvamv9BApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.cvvamv9b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cvvamv9BApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cvvamv9BApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mx/edu/utez/cvvamv9b/Cvvamv9BApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.cvvamv9b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cvvamv9BApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
